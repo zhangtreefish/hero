62ba2838e6a198730f2ceeebf3e111f72dcb3b3f
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-06-21 18:50:41 -0500

    refactor

--- app/project.component.ts
+++ app/project.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input } from '@angular/core';
 //import {Project} from './project.service';
 import { Project } from './project';
-//import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';
 
 @Component({
 	selector: 'details',
-	templateUrl: 'app/project.component.html'
+	templateUrl: 'app/project.component.html',
+	styleUrls: ['app/project.component.css']
 })
 export class ProjectComponent {
 	@Input()
 	project: Project;
-	// constructor(projectsComponent: ProjectsComponent) {
-	// 	this.project = projectsComponent.getActiveProject();
-	}
+	// constructor(appComponent: AppComponent) {
+	// 	this.project = appComponent.selectedProject;
+	// }
