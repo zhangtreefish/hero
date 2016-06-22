c9f41f2b1d2f7c3695ec32c8f9d0e3a9ed35e2be
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-06-22 11:08:22 -0500

    feat: add router to app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,21 @@
 import {Component} from '@angular/core';
 import { ProjectsComponent } from './projects.component';
 import { ProjectService } from './project.service';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
-	directives: [ProjectsComponent],
-	providers: [ProjectService]
+	directives: [ROUTER_DIRECTIVES],
+	providers: [ROUTER_PROVIDERS, ProjectService]
 })
-
+@RouteConfig([
+	{
+		path: '/projects',
+		name: 'Projects',
+		component: ProjectsComponent
+	}
+])
 export class AppComponent {
 	title = 'Tour of Projects';
 }
