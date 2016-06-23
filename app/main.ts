32915394966911d1de37fe3abd8c103aa02463dd
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-06-22 22:35:15 -0500

    feat: use Http and in-memory web api to load projects data

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,15 @@
+import { XHRBackend } from '@angular/http';
+
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }               from './in-memory-data.service';
+
 import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {HTTP_PROVIDERS} from '@angular/http';
+
 import {AppComponent} from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+    HTTP_PROVIDERS,
+    { provide: XHRBackend, useClass: InMemoryBackendService },
+    { provide: SEED_DATA, useClass: InMemoryDataService }
+]);
--- app/project.service.ts
+++ app/project.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@angular/core';
-import { PROJECTS } from './projects';
+import { Http } from '@angular/http';
+// import { PROJECTS } from './projects';
+import 'rxjs/add/operator/toPromise';
+
+import { Project } from './project';
 
 @Injectable()
 export class ProjectService {
-	getProjects() {
-		return Promise.resolve(PROJECTS);
+	private _projectsUrl = 'app/projects';
+
+	constructor(private _http: Http) { };
+
+	getProjects(): Promise<Project[]> {
+		return this._http.get(this._projectsUrl)
+		.toPromise()
+		.then(response => response.json().data)
+		.catch(this._handleError);
 	}
+
+	private _handleError(error: any) {
+		console.error('An error occurred', error);
+		return Promise.reject(error.message || error);
+	}
+
 	getProject(id: number) {
 		return this.getProjects().then(projects => projects.filter(project => project.id===id)[0]);
 	}
