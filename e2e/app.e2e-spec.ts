f2a7e8dd038ec915fdd68b59e9e6eac9b6e7495a
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-06-24 12:07:29 -0500

    test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,13 @@
 
 describe('QuickStart E2E Tests', function () {
 
-	var expectedMsg = 'My First Angular 2 App';
+    var expectedMsg = 'Tour of Projects';
 
+    beforeEach(function () {
+        browser.get('');
+    });
 
-  beforeEach(function () {
-    browser.get('');
-  });
-
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
-  });
-
+    it('should display: ' + expectedMsg, function () {
+        expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+    });
 });
