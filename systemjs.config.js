d83cb5fade4fb6f98df359205c7cb5e7531089dc
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-06-23 23:52:23 -0500

    fix

--- systemjs.config.js
+++ systemjs.config.js
@@ -10,7 +10,9 @@
 
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs'
+    'rxjs':                       'node_modules/rxjs',
+    'moment':                     'vendor/moment/moment.js',
+    'ng2-bootstrap':              'vendor/ng2-bootstrap'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -18,6 +20,8 @@
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    'ng2-bootstrap':              { defaultExtension: 'js' },
+    'moment':                     { format: 'cjs' }
   };
 
   var ngPackageNames = [
