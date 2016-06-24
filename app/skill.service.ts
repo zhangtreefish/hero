11602edc5c0fa3f6cda0fda97e552f615d5a51bf
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-06-24 14:54:28 -0500

    docs

--- app/skill.service.ts
+++ app/skill.service.ts
@@ -4,7 +4,7 @@ export class SkillService {
 		return ["HTML5", "CSS3", "Bootstrap", "JavaScript", "TypeScript", "GitHub", "Gulp",
 			"Object-Oriented Programming", "jQuery", "AngularJS", "Angular2", "D3", "KnockoutJS",
 			"web performance optimization", "Agile software development and scrum methodology",
-			"Unit Test with JasmineJS", "Selenium", "WebDriverIO", "Node.js", "Underscore.js",
+			"JasmineJS", "Selenium", "WebDriverIO", "Node.js", "Underscore.js",
 			"Python", "SQLAlchemy", "Flask", "Jinja2", "OAuth 2.0", "Google App Engine",
 			"Ubuntu linux server", "SQL", "Excel", "Blackboard Learn"];
 	};
